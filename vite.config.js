ccd9c546d13de4b56ac50c08279aba8bea581f84
ddelgado25 <daviddelgado2515@gmail.com>
2025-07-11 14:46:42 -0400

    Clean cached files and add .nojekyll for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/delgadodevelops-portfolio/', // must match your repo name
+  base: '/delgadodevelops-portfolio/', // this must match your repo name exactly
 });
