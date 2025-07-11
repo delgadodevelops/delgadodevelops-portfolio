8d5e58e77475a3f1f8d0ddad52bc6afcc6e7b7ae
ddelgado25 <daviddelgado2515@gmail.com>
2025-07-11 15:00:22 -0400

    Deploy latest build to gh-pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/delgadodevelops-portfolio/', // this must match your repo name exactly
-});
